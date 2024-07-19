00d23091401f01bab773940c4a8cc4c32d7ef176
akihiro-mizun <mizuno@gl-s.co.jp>
2024-07-19 13:27:27 +0900

    test

--- src/patternA/Sample001.tsx
+++ src/patternA/Sample001.tsx
@@ -1,25 +1,72 @@
 import { useEffect , useState } from 'react';
+import { useLocation } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useQRCode } from 'next-qrcode';
 
 import { db } from "../firebase";
-import { collection, query, doc, getDoc, getDocs } from "firebase/firestore";
+import styles from "./Sample001.module.css";
 
 function  Sample001() {
-  const [id, setId] = useState('');
+  const [companyName, setCompanyName] = useState('');
   const [userName, setUserName] = useState('');
+  const [post, setPost] = useState('');
+  const [postNo, setPostNo] = useState('');
+  const [address1, setAddress1] = useState('');
+  const [address2, setAddress2] = useState('');
+  const [telNo, setTelNo] = useState('');
+  const [url, setURL] = useState('');
 
+  const { Canvas } = useQRCode();
+  const search = useLocation().search;
+
+  // 初回のみ実行
   useEffect(() => {
-    const q = query(collection(db, "UserInfo") );
-    getDocs(q).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        setId(doc.id);
-        setUserName(doc.data().UserName);
-      });
-    }); 
+
+    // URL引数取得
+    const urlquery = new URLSearchParams(search);
+    let dId= urlquery.get('id');
+    if (dId === null || dId === '') {
+      dId = '00000000000000000000'
+    }
+
+    const q = doc(db, "UserInfo", dId );
+    getDoc(q).then((userInfoResult) => {
+      setCompanyName(userInfoResult!.data()!.CompanyName);
+      setUserName(userInfoResult!.data()!.UserName);
+      setPost(userInfoResult!.data()!.Post);
+      setPostNo(userInfoResult!.data()!.PostNo);
+      setAddress1(userInfoResult!.data()!.Address1);
+      setAddress2(userInfoResult!.data()!.Address2);
+      setTelNo(userInfoResult!.data()!.TelNo);
+      setURL(userInfoResult!.data()!.Url);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
   return (
-    <div className="Sample001">
-      <div>{userName}</div>
+    <div className={styles.tmpImageBGI}>
+      <div className={ styles.companyNameStyle }>{ companyName }</div>
+      <div className={ styles.brockStyle }>
+        <div className={ styles.postStyle }>{post}：</div>
+        <div className={ styles.userNameStyle }>{ userName }</div>
+      </div>
+      <div>
+        <div className={ styles.postNoStyle }>〒{postNo}</div>
+        <div className={ styles.address1Style }>{address1}</div>
+        <div className={ styles.address2Style }>{address2}</div>
+        <div className={ styles.telNoStyle }>Tel：{telNo}</div>
+        <div className={ styles.urlStyle }>
+        <Canvas
+            text={ url }
+            options={{ 
+              type: 'image/png',
+              quality: 0.90, 
+              width: 50,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/patternA/Sample002.tsx
+++ src/patternA/Sample002.tsx
@@ -1,8 +1,55 @@
+import { useEffect , useState } from 'react';
+import { useLocation } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useQRCode } from 'next-qrcode';
+
+import { db } from "../firebase";
+import styles from "./Sample002.module.css";
+
 
 function  Sample002() {
+  const [companyName, setCompanyName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [post, setPost] = useState('');
+  const [postNo, setPostNo] = useState('');
+  const [address1, setAddress1] = useState('');
+  const [address2, setAddress2] = useState('');
+  const [telNo, setTelNo] = useState('');
+  const [url, setURL] = useState('');
+
+  const { Canvas } = useQRCode();
+  const search = useLocation().search;
+
+  // 初回のみ実行
+  useEffect(() => {
+
+    // URL引数取得
+    const urlquery = new URLSearchParams(search);
+    let dId= urlquery.get('id');
+    if (dId === null || dId === '') {
+      dId = '00000000000000000000'
+    }
+
+    const q = doc(db, "UserInfo", dId );
+    getDoc(q).then((userInfoResult) => {
+      setCompanyName(userInfoResult!.data()!.CompanyName);
+      setUserName(userInfoResult!.data()!.UserName);
+      setPost(userInfoResult!.data()!.Post);
+      setPostNo(userInfoResult!.data()!.PostNo);
+      setAddress1(userInfoResult!.data()!.Address1);
+      setAddress2(userInfoResult!.data()!.Address2);
+      setTelNo(userInfoResult!.data()!.TelNo);
+      setURL(userInfoResult!.data()!.Url);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
     return (
+      <div className={styles.tmpImageBGI}>
       <div className="Sample002">
         <div>Hello World Sample002</div>
+      </div>
+      <div className={ styles.companyNameStyle }>{ companyName }</div>
       </div>
     );
   };
