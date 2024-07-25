2940c1dc0899554a96a7e0d7fe80da849fcaa510
akihiro-mizun <mizuno@gl-s.co.jp>
2024-07-25 15:03:43 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
+import A000 from './patternA/Sample000';
 import A001 from './patternA/Sample001';
 import A002 from './patternA/Sample002';
 import B101 from './patternB/Sample101';
@@ -14,6 +15,7 @@ function App() {
   <BrowserRouter>
     <Routes>
       <Route path={`/`} element={<Home />} />
+      <Route path={`/patternA000`} element={<A000 />} />
       <Route path={`/patternA001`} element={<A001 />} />
       <Route path={`/patternA002`} element={<A002 />} />
       <Route path={`/patternB101`} element={<B101 />} />
